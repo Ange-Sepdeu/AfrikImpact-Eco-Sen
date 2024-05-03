39e6e2c75f2ed0c73f19eb459446cd03d206e773
Ange-Sepdeu <chriskameni25@gmail.com>
2024-05-03 12:47:14 +0100

    Modified axiosInstance

--- src/axiosInstance/axiosInstance.js
+++ src/axiosInstance/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL: "https://afrik-impact-api-44cy-h5e8a859d-ange-sepdeus-projects.vercel.app",
+    baseURL: "https://afrikimpact-api.onrender.com",
     headers: {
         "Content-Type": "application/json",
         Accept: "application/json",

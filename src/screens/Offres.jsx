39e9ade299bb0787ddbd64d409951ab07204c3a2
Ange-Sepdeu <chriskameni25@gmail.com>
2024-05-18 22:39:57 +0100

    Applied modifications and remarks

--- src/screens/Offres.jsx
+++ src/screens/Offres.jsx
@@ -11,21 +11,24 @@ import {getCountryCode, getCountryData, getCountryDataList} from "countries-list
 export default function Offres() {
     const [amountDetail, setAmountDetail] = useState([
         {
-            amount: "45",
+            amount: "150",
             active: false
         },
         {
-            amount: "70",
+            amount: "100",
             active: false
         },
         {
-            amount: "90",
+            amount: "250",
             active: false
         }
     ])
     const [countries, setCountries] = useState([])
     const [activeAmount, setActiveAmount] = useState();
-    const [btn, setBtn] = useState(false);
+    const [selectedAmount, setSelectedAmount] = useState("")
+    const [inputAmount, setInputAmount] = useState("")
+    const [dropdownActive, setDropdownActive] = useState(false);
+    const [textInputActive, setTextInputActive] = useState(false);
     const handleSetAmount = (amt) => {
         setActiveAmount(amt);
         let amount = [...amountDetail];
@@ -60,6 +63,16 @@ export default function Offres() {
         const countriesList = getCountryDataList();
         setCountries(countriesList);
     })
+    useEffect(() => {
+        if (selectedAmount.trim().length > 0)
+            setTextInputActive(true)
+        else
+            setTextInputActive(false)
+        if(inputAmount.trim().length > 0)
+            setDropdownActive(true)
+        else
+            setDropdownActive(false)
+    }, [selectedAmount, inputAmount])
   return (
     <div className='h-[120vh]'>
         <img src={bgOffer} alt="Background offer image" className='w-full h-full fixed resize-contain'/>
@@ -81,23 +94,28 @@ export default function Offres() {
                     1. votre don
                 </div>
                 <div className='p-4 w-full'>
-                    <h2 className='text-center text-[18px] font-semibold bg-green-800 text-white p-4 rounded-[10px] m-auto w-[50%]'>Choisissez le montant de votre offre</h2>
-                    <button onClick={() => setBtn(!btn)} className='p-4 text-center m-auto'>{btn ? "Afficher les choix" : "Afficher montant libre"}</button>
-                    { !btn ?
-                    <div className="flex flex-row justify-between items-center mt-2">
+                    <h2 className='text-center text-[18px] font-semibold bg-green-800 text-white p-4 rounded-[10px] m-auto w-[50%]'>Choisissez ou inscrivez le montant de votre offre</h2>
+                    {/* <button onClick={() => setBtn(!btn)} className='p-4 text-center m-auto'>{btn ? "Afficher les choix" : "Afficher montant libre"}</button> */}
+                    <div className="mt-10">
+                        <select onChange={(e) => setSelectedAmount(e.target.value)} disabled={dropdownActive} className='w-full outline-none p-2 bg-blue-800 bg-opacity-70 text-white text-[24px]' name="" id="">
+                        <option defaultChecked value="">Choisir</option>
                         {
                             [...amountDetail].map((amt, index) => (
+                                <option>
                                 <div key={index} onClick={() => handleSetAmount(amt)} 
                                 className={`cursor-pointer p-2 w-[32%] text-center text-[20px] ${(activeAmount?.amount===amt.amount) ? "text-white bg-green-800": "border border-gray-400"}  rounded-[5px]`}>
-                                    {amt.amount}
+                                    {amt.amount} €
                                 </div>
+                                </option>
                             ))
                         }
+                        </select>
+                        <div className='my-5 font-bold text-center w-full text-[20px]'>OU</div>
+                        <div className='flex border border-gray-400 rounded-[7px] flex-row justify-between items-center mt-2 w-full m-auto'>
+                        <input disabled={textInputActive} type="number" min={1} onChange={(e) => setInputAmount(e.target.value)} className='p-3 ml-1 outline-none text-[20px] w-[80%]' placeholder='Montant Libre' id="" />
+                        <div className='rounded-r-[7px] text-[20px] p-3 text-center text-gray-400 bg-gray-200 border-l border-gray-400 w-[15%]'>€</div>
                     </div>
-                    :<div className='flex border border-gray-400 rounded-[7px] flex-row justify-between items-center mt-2 w-[95%] m-auto'>
-                        <input type="number" min={1} onChange={(e) => handleSetMontantLibre(e.target.value)} className='p-3 ml-1 outline-none text-[20px] w-[80%]' placeholder='Montant Libre' id="" />
-                        <div className='rounded-r-[7px] text-[20px] p-3 text-center text-gray-400 bg-gray-200 border-l border-gray-400 w-[15%]'>$</div>
-                    </div>}
+                    </div>
                 </div>
             </div>
             <div className='bg-white h-full w-full sm:w-full md:w-full lg:w-[32%]'>
@@ -134,7 +152,6 @@ export default function Offres() {
                     <select className='p-3 uppercase mt-3 w-full outline-none border border-gray-400'>
                         {
                             [...countries].map((singleCountry, index)=> {
-                                console.log(getFlagEmoji(getCountryCode(singleCountry.name)))
                                 return(
                                 <option value={singleCountry.name} key={index}> {getFlagEmoji(getCountryCode(singleCountry.name))} {singleCountry.name}</option>
                                 )})
